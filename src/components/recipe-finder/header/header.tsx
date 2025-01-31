3fc7c16320dbe590e152843e22ef974d19d1cb68
SANNNNN-123 <zuhairaziz95@yahoo.com>
2025-01-31 16:55:00 +0800

    add subtitle

--- src/components/recipe-finder/header/header.tsx
+++ src/components/recipe-finder/header/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <div className="text-center mb-16 px-2 py-0">
-      <h1 className="max-w-2xl mx-auto tracking-tight mb-8">
+    <div className="text-center mb-4">
+      <h1 className="max-w-2xl mx-auto tracking-tight mb-4">
         <div className="text-center text-4xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
           Masak Senang
           <br />
@@ -10,6 +10,7 @@ const Header = () => {
             Resepi GPT
           </span>
         </div>
+        <div className="text-center text-lg text-muted-foreground mt-2">Percuma. Sumber Terbuka. Penjana Resepi Masakan Melayu.</div> 
       </h1>
     </div>
   )
@@ -17,11 +18,3 @@ const Header = () => {
 
 export default Header
 
-{/* <div className="text-3xl font-bold mb-2 text-black dark:text-white">
-  Tak Perlu Pening Fikir Nak Masak Apa with
-</div>
-<div className="text-5xl font-extrabold">
-  <span className="bg-gradient-to-r from-purple-400 via-blue-400 to-cyan-400 text-transparent bg-clip-text">
-    Resepi GPT
-  </span>
-</div> */}
